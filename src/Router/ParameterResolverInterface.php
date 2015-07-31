4cb61462487d484339bad480f4b09e64dd2f0f3d
Iltar van der Berg <ivanderberg@hostnet.nl>
2015-07-31 10:46:01 +0200

    Draft of ParameterResolverInterface

---
+++ src/Router/ParameterResolverInterface.php
@@ -0,0 +1,33 @@
+<?php
+namespace Iltar\HttpBundle\Router;
+
+/**
+ * Works as a reversed ParameterConverter from the SensioFrameworkExtraBundle.
+ *
+ * @author Iltar van der Berg <kjarli@gmail.com>
+ */
+interface ParameterResolverInterface
+{
+    /**
+     * Check if the parameter is supported.
+     *
+     * @param string $name   Name of the parameter as defined in the Route
+     * @param mixed  $value  Value of the parameter as filled in
+     * @return bool
+     */
+    public function supportsParameter($name, $value);
+
+    /**
+     * Resolves a parameter to a string.
+     *
+     * Example:
+     *  - Route: "/profile/{username}/"
+     *  - generate("app.profile.view", ["username" => $user]
+     *  - { return $user->getUsername(); }
+     *
+     * @param string $name   Name of the parameter as defined in the Route
+     * @param mixed  $value  Value of the parameter as filled in
+     * @return string
+     */
+    public function resolveParameter($name, $value);
+}
