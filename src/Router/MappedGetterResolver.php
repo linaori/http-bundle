d40c8fd50c0eb9e98e6bb2a8d1df1d4c92a2d1e3
Iltar van der Berg <ivanderberg@hostnet.nl>
2015-08-17 10:33:02 +0200

    Added the MappedGetterResolver

---
+++ src/Router/MappedGetterResolver.php
@@ -0,0 +1,67 @@
+<?php
+namespace Iltar\HttpBundle\Router;
+
+use Iltar\HttpBundle\Exception\UncallableMethodException;
+
+/**
+ * Resolves anything that's mapped.
+ *
+ * Example:
+ *  - App\User.username : getUsername # grab the username if the key is username
+ *  - App\User          : getid       # Grab getId if nothing more specific is defined
+ *  - App\Post          : getSlug     # Always grab getSlug
+ *
+ * Arguments are passed via the constructor in an array: [
+ *    ['App\User' => [
+ *      'username' => 'getUsername',
+ *       null      => 'getId',
+ *    ],
+ *    ['App\Post' => [null, 'getSlug']],
+ *  ]
+ *
+ * @author Iltar van der Berg <kjarli@gmail.com>
+ */
+class MappedGetterResolver implements ParameterResolverInterface
+{
+    /**
+     * @var array
+     */
+    private $mapping;
+
+    /**
+     * @param array $mapping
+     */
+    public function __construct(array $mapping)
+    {
+        $this->mapping = $mapping;
+    }
+
+    /**
+     * Supports anything that's mapped.
+     *
+     * {@inheritdoc}
+     */
+    public function supportsParameter($name, $entity)
+    {
+        $part = $this->mapping[get_class($entity)];
+        return isset($part[$name]) || isset($part[null]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolveParameter($name, $entity)
+    {
+        $part     = $this->mapping[get_class($entity)];
+        $method   = isset($part[$name]) ? $part[$name] : $part[null];
+        $callable = [$entity, $method];
+
+        if (!is_callable($callable)) {
+            throw new UncallableMethodException(
+                sprintf('Method %s::%s is expected to be callable for %s.', get_class($entity), $method, self::class)
+            );
+        }
+
+        return (string) $callable();
+    }
+}
